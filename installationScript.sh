93f27031ad7b29b2f3eef06533ab4d13b3d8be6d
Arch <aurichiio@icloud.com>
2021-07-21 22:34:48 -0700

    Made a change in the script to reflect it's not being worked on anymore.

--- installationScript.sh
+++ installationScript.sh
@@ -3,8 +3,19 @@
 APP_PATH="/Library/Application Support/WLAN"
 RES_PATH="/Library/Application Support/WLAN/StatusBarApp.app/Contents/Resources/"
 
+echo
+echo
+echo
+
+echo "WARNING: The script is now discontinued. I won't be updating it anymore."
+
+echo
+echo
+echo
+
+echo "Choosing Dark Mode will apply the white icon set and choosing Light Mode will apply the black icon set!"
 echo "Do you wish to:"
-echo "Install (D)ark Mode| Install (L)ight Mode| (Q)uit"
+echo "Install [D]ark Mode | Install [L]ight Mode | [Q]uit"
 read -p ": " answer
 case ${answer:0:1} in
 	d|D )
